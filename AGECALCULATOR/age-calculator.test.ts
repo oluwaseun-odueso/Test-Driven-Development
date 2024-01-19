c21bdcf20a61675fc3f53250c17ba7b59bcf4056
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2024-01-19 12:33:24 +0100

    Handled more cases for age calculator

--- AGECALCULATOR/age-calculator.test.ts
+++ AGECALCULATOR/age-calculator.test.ts
@@ -26,4 +26,42 @@ describe('agecalculator', () => {
          expect(actual).toBe(expected)
       })
    })
+
+   describe('has not had birthday this year', () => {
+      describe('birthday in later month', () => {
+         test.each([
+            {birthDate: '2000/09/05', targetDate: '2024/06/30', expected: 23},
+            {birthDate: '2006/06/16', targetDate: '2024/01/19', expected: 17},
+            {birthDate: '2004/02/29', targetDate: '2005/03/01', expected: 1},
+         ])('birth date: $birthDate, target date: $targetDate, expected: $expected', 
+         ({birthDate, targetDate, expected}) => {
+            // Arrange
+            const sut = createAgeCalculator();
+      
+            // Act
+            const actual = sut(new Date(birthDate), new Date(targetDate))
+      
+            // Assert
+            expect(actual).toBe(expected)
+         })
+      })
+      
+      describe('birthday in same month', () => {
+         test.each([
+            {birthDate: '1998/06/30', targetDate: '2024/06/19', expected: 25},
+            {birthDate: '1957/10/30', targetDate: '2024/10/19', expected: 66},
+         ])('birth date: $birthDate, target date: $targetDate, expected: $expected', 
+         ({birthDate, targetDate, expected}) => {
+            // Arrange
+            const sut = createAgeCalculator();
+      
+            // Act
+            const actual = sut(new Date(birthDate), new Date(targetDate))
+      
+            // Assert
+            expect(actual).toBe(expected)
+         })
+      })
+   })
+   
 })
--- AGECALCULATOR/age-calculator.ts
+++ AGECALCULATOR/age-calculator.ts
@@ -1,5 +1,11 @@
 export function createAgeCalculator() {
    return function (birthDate: Date, targetDate: Date) {
-      return targetDate.getFullYear() - birthDate.getFullYear()
+      const differenceInYears = targetDate.getFullYear() - birthDate.getFullYear()
+      if (birthDate.getMonth() > targetDate.getMonth() ||
+      (birthDate.getMonth() == targetDate.getMonth() && birthDate.getDate() > targetDate.getDate())) {
+         return differenceInYears - 1
+      }
+      // if (birthDate.getMonth() == targetDate.getMonth() && birthDate.getDate() > targetDate.getDate()) return differenceInYears - 1
+      return differenceInYears
    }
 }
