69fdd944b5d686ac06968854789eb3cb1b8818c9
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2024-02-05 16:43:16 +0100

    More tests

--- CSVFILEKATA/customer-csv-file-writer.test.ts
+++ CSVFILEKATA/customer-csv-file-writer.test.ts
@@ -5,20 +5,38 @@ import { CustomerCsvFileWriter } from "./customer-csv-file-writer";
 import { FileWriter } from "./file-writer";
 
 describe('CustomerCsvFileWriter', () => {
-  test('?', () => {
-    // Arrange
-    const customer = new Customer(
-      "Peter Wile",
-      "12787939829"
-    )
-    const fileWriter: FileWriter = {
-      writeline: jest.fn()
-    }
-    const sut = new CustomerCsvFileWriter(fileWriter)
-    // Act
-    sut.writeCustomers("", [customer])
-    // Assert
-    expect(fileWriter.writeline).toHaveBeenCalledTimes(1);
-    expect(fileWriter.writeline).toBeCalledWith("", "Peter Wile,12787939829")
+  describe('one customer', () => {
+    test.each([
+      {customer: new Customer("Peter Wiles", "12787939829"), expected: "Peter Wiles,12787939829"},
+      {customer: new Customer("Larry Page", "32773097753874"), expected: "Larry Page,32773097753874"}
+    ])('customer: $expected', ({customer, expected}) => {
+      // Arrange
+      const fileWriter: FileWriter = {
+        writeline: jest.fn()
+      }
+      const sut = new CustomerCsvFileWriter(fileWriter)
+      // Act
+      sut.writeCustomers("customers.csv", [customer])
+      // Assert
+      expect(fileWriter.writeline).toHaveBeenCalledTimes(1);
+      expect(fileWriter.writeline).toBeCalledWith("customers.csv", expected)
+    })
+  })
+
+  describe('two customers', () => {
+    test('should write both customers', () => {
+      // Arrange
+      const customers = [new Customer("Peter Wiles", "12787939829"), new Customer("Larry Page", "32773097753874")]
+      const fileWriter: FileWriter = {
+        writeline: jest.fn()
+      }
+      const sut = new CustomerCsvFileWriter(fileWriter)
+      // Act
+      sut.writeCustomers("cust.csv", customers)
+      // Assert
+      expect(fileWriter.writeline).toHaveBeenCalledTimes(2);
+      expect(fileWriter.writeline).toBeCalledWith("cust.csv", "Peter Wiles,12787939829")
+      expect(fileWriter.writeline).toBeCalledWith("cust.csv", "Larry Page,32773097753874")
+    })
   })
 })
--- CSVFILEKATA/customer-csv-file-writer.ts
+++ CSVFILEKATA/customer-csv-file-writer.ts
@@ -5,6 +5,9 @@ export class CustomerCsvFileWriter {
   constructor (private fileWriter: FileWriter) {}
 
   writeCustomers(fileName: string, customers: Customer[]) {
-    this.fileWriter.writeline("",  "Peter Wile,12787939829")
+    this.fileWriter.writeline(fileName, `${customers[0].name},${customers[0].contactNumber}`)
+    if (customers.length > 1) {
+      this.fileWriter.writeline(fileName, `${customers[1].name},${customers[1].contactNumber}`)
+    }
   }
 }
